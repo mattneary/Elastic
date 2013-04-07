585fa702be938ab9e4a486dcabcbcbe35aa00853
Matt Neary <neary.matt@gmail.com>
2013-04-07 04:40:26 -0400

    clean up

--- pull-refresh.js
+++ pull-refresh.js
@@ -13,26 +13,35 @@ $.fn.pullToRefresh = function(refreshCallback) {
 	var scrollCount = 0;
 	var isRefreshing = false;
 	
+	// set down arrow as banner default state
 	$banner.html("&#x21A1;");
 	$wrapper.on("mousewheel", function(evt) {
 		if(evt.originalEvent.wheelDelta > 0 && $wrapper.scrollTop() <= 100) {				
+			// keep track of scroll progress manually
 			scrollCount++;
-			if( isRefreshing ) return;			// don't handle scrolls when refreshing			
+			// ignore scrolls while refreshing
+			if( isRefreshing ) return;
 			
 			$wrapper[0].scrollTop -= scrollCount/1e2;			
 			
 			if( $wrapper.scrollTop() <= 0 ) {		
+				// when refresh has been triggered display up arrow
 				isRefreshing = true;
-				$banner.html("&#x219F;");		// Up arrow								
+				$banner.html("&#x219F;");
 				
+				// after a brief delay change to refresh symbol and refresh
 				setTimeout(function() {
-					$banner.html("&#x21BB;");	// Refreshing symbol					
+					$banner.html("&#x21BB;");
 					
+					// perform refresh action and pass finish callback
 					refresh(function() {
-						$banner.html("&#x21A1;");	// Down arrow
+						// display down arrow and return scroll position
+						$banner.html("&#x21A1;");
 						$wrapper[0].scrollTop = 100;
 					});
 				}, options.waitBeforeDisplayingRefresh);
+				
+				// delay listening when refreshing
 				setTimeout(function() {					
 					isRefreshing = false; 										
 				}, options.waitBeforeAcceptingNew);
